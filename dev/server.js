276108c3a45d0cd0688f9549fd6e66aff3185d64
Tom Hummel <tphummel@gmail.com>
2014-08-23 10:14:35 -0700

    ;-)

--- dev/server.js
+++ dev/server.js
@@ -1,29 +1,29 @@
-var http = require("http"),
-    path = require("path"),
-    fs   = require("fs"),
-    url  = require("url");
+var http = require("http")
+var path = require("path")
+var fs   = require("fs")
+var url  = require("url")
 
-var port = process.env.PORT || 7007;
+var port = process.env.PORT || 7007
 
 http.createServer(function(req, res){
-  var reqUrl = url.parse(req.url);
+  var reqUrl = url.parse(req.url)
 
-  console.log("request heard: ", new Date, reqUrl.pathname);
+  console.log("request heard: ", new Date, reqUrl.pathname)
 
   if(reqUrl.pathname === "/"){
-    res.writeHead(200, {"Content-Type": "text/html"});
-    fs.createReadStream(path.join(__dirname,'../build/index.html')).pipe(res);
+    res.writeHead(200, {"Content-Type": "text/html"})
+    fs.createReadStream(path.join(__dirname,'../build/index.html')).pipe(res)
 
   }else if(reqUrl.pathname === "/main.js"){
-    res.writeHead(200, {"Content-Type": "application/javascript"});
-    fs.createReadStream(path.join(__dirname,'../build/main.js')).pipe(res);
+    res.writeHead(200, {"Content-Type": "application/javascript"})
+    fs.createReadStream(path.join(__dirname,'../build/main.js')).pipe(res)
 
   }else{
-    res.writeHead(404, {"Content-Type": "text/plain"});
-    res.write("file not found");
-    res.end();
+    res.writeHead(404, {"Content-Type": "text/plain"})
+    res.write("file not found")
+    res.end()
   }
 
-}).listen(port);
+}).listen(port)
 
-console.log("dev server listening on port:", port);
+console.log("dev server listening on port:", port)
